a8320b97585092bfbddfdbb268954bfe23457d21
Ryan Nowak <nowakra@gmail.com>
2018-10-23 20:05:14 -0700

    Responding to feedback

--- src/DummyFileBasedCms.Web/FlatFileCmsGitOptions.cs
+++ src/DummyFileBasedCms.Web/FlatFileCmsGitOptions.cs
@@ -1,12 +1,19 @@
-﻿namespace DummyFileBasedCms.Web
+﻿using System.Collections.Generic;
+
+namespace DummyFileBasedCms.Web
 {
     public class FlatFileCmsGitOptions
     {
-        public string FilePath { get; set; }
-        public string RepositoryUrl { get; set; }
-        public string Branch { get; set; }
-        public string PublicKey { get; set; }
-        public string PrivateKey { get; set; }
-        public string Passphrase { get; set; }
+        public List<Directory> Directories { get; set; } = new List<Directory>();
+
+        public class Directory
+        {
+            public string FilePath { get; set; }
+            public string RepositoryUrl { get; set; }
+            public string Branch { get; set; }
+            public string PublicKey { get; set; }
+            public string PrivateKey { get; set; }
+            public string Passphrase { get; set; }
+        }
     }
 }
